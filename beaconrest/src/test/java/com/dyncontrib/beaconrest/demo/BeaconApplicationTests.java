05fb27c9ed60c154504eff1bd47c0237ca56c395
Micheal Myers <michealmyers@Micheals-MacBook-Pro.local>
2019-10-19 18:50:40 -0500

    Added Beacon REST inital commit

---
+++ beaconrest/src/test/java/com/dyncontrib/beaconrest/demo/BeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyncontrib.beaconrest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
